62c1b8d25b1b484ddfa8f9e79fb70938f71817e7
Juanma <juanmanujel@gmail.com>
2021-10-14 09:57:41 +0200

    update

--- PRA2/UPDATE.sql
+++ PRA2/UPDATE.sql
@@ -1,1 +1,11 @@
-UDPDATE 
+UPDATE USUARIO1
+SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester'; 
+
+UPDATE AULA2
+SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
+
+UPDATE CURSO3
+SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
+
+UPDATE EDIFICIO4
+SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
