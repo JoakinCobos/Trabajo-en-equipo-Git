538df585348ca7564d5cc80d1e452a69dfbc3201
Juanma <juanmanujel@gmail.com>
2021-10-14 10:04:44 +0200

    update final

--- UPDATE.sql
+++ UPDATE.sql
@@ -2,10 +2,10 @@ UPDATE USUARIO1
 SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester'; 
 
 UPDATE AULA2
-SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
+SET Nombre = 'Aula', Numero= '9', Planta = '1', Descripción = 'Inglés';
 
 UPDATE CURSO3
-SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
+SET Código = '99', Nombre= 'Matemáticas', Matemáticas académicas. = '50', Horas = '100';
 
 UPDATE EDIFICIO4
-SET Nombre = 'Pepe', Apellido= 'Guardiola', Edad = '50', Dirección = 'Manchester';
+SET Nombre = 'Empires States', Descripción= 'Muy alto';
